86c37e5e1f71a5a876297e50b64e5c17ace8cbc7
Rowan Schaaf <rowan.schaaf@gmail.com>
2026-02-27 16:40:59 +1300

    fix: add apple-events entitlement and @MainActor to AppDelegate

--- MemtimeHelper/MemtimeHelper/AppDelegate.swift
+++ MemtimeHelper/MemtimeHelper/AppDelegate.swift
@@ -1,5 +1,6 @@
 import AppKit
 
+@MainActor
 class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ notification: Notification) {
         // Suppress dock icon (belt-and-suspenders alongside LSUIElement)
